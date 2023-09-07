763b140f253992ced0888da6565645e6b1b3078e
Valton <izzz.toni@gmail.com>
2023-09-07 07:58:28 +0200

    updated footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,8 +4,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <div>
-      <div className='relative flex bg-[#111111] h-32 justify-center items-center'>
-        <p className='max-big:hidden absolute left-5 font-sans font-medium'>Created By <span className='text-[orange]'>- Valton Kasami</span></p>
+      <div className='flex bg-[#111111] h-32 justify-center items-center'>
     <div className='flex space-x-7 items-center justify-center'>
         <a href='/about'><h1 className='text-xl hover:text-[orange]'>About</h1></a>
         <a className='max-sm:hidden' href='#'><h1 className='text-xl hover:text-[orange]'>News</h1></a>
@@ -14,7 +13,6 @@ export const Footer = () => {
         
         <a href='/contact'><h1 className='text-xl hover:text-[orange]'>Contact</h1></a>
     </div>
-    <p className='font-sans font-medium absolute right-5 max-big:hidden'>@2023 - All rights are reserved</p>
     </div>
     </div>
   )
